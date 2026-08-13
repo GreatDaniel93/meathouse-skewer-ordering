5475286b102a3989c45106ca35a45af139705ddc
GreatDaniel93 <danielchen993@gmail.com>
2026-08-13 12:09:50 +1000

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Meat House Skewer Ordering',description:'Meat House Canberra unlimited skewer ordering system'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
