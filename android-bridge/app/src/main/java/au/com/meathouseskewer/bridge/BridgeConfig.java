b14acbae4993d3abaf38f51b8ca8d11526c6fcdb
GreatDaniel93 <danielchen993@gmail.com>
2026-08-14 12:43:23 +1000

    Embed Meat House bridge credential in APK config

--- android-bridge/app/src/main/java/au/com/meathouseskewer/bridge/BridgeConfig.java
+++ android-bridge/app/src/main/java/au/com/meathouseskewer/bridge/BridgeConfig.java
@@ -3,6 +3,7 @@ package au.com.meathouseskewer.bridge;
 final class BridgeConfig {
     static final String SUPABASE_URL = "https://tbpinwmfmywkpdqnpula.supabase.co";
     static final String SUPABASE_KEY = "sb_publishable_bW5xqejyr2xhqsnrDhLDNw_O1mJMQzV";
+    static final String BRIDGE_KEY = "mhb_6avkCWPntpEUx9TnTCTxTs8ka-5AIBio";
     static final String PREFS = "bridge";
     private BridgeConfig() {}
 }
